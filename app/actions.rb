3c3d852fcc03174232600080664d707fabbc2c63
Steven Hanna <abstracthanna@gmail.com>
2015-11-10 14:42:43 -0500

    Revert "breakout lessons"
    This reverts commit e0f2eddec1679d3decc141db5875ea4ee5b38b60.

--- app/actions.rb
+++ app/actions.rb
@@ -1,48 +1,4 @@
-helpers do
-  def current_user
-    User.find(session[:user]) if session[:user]
-  end
-end
-
+# Homepage (Root path)
 get '/' do
   erb :index
-end
-
-get '/messages/new' do
-  erb :'messages/new'
-end
-
-get '/users/new' do
-  @user = User.new
-  erb :'users/new'
-end
-
-get '/login' do
-  erb :'sessions/new'
-end
-
-post '/login' do
-  user = User.find_by(username: params[:username])
-  if user && user.password == params[:password]
-    redirect '/'
-  else
-    @error = 'invalid username or password'
-    erb :'sessions/new'
-  end
-end
-
-get '/logout' do
-  session.delete(:user)
-  redirect '/'
-end
-
-post '/users' do
-  @user = User.new(params)
-
-  if @user.save
-    session[:user] = @user.id
-    redirect '/'
-  else
-    erb :'users/new'
-  end
 end
